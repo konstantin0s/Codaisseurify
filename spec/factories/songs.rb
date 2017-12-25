2858c800f46ad7fbb30a009d44a7aae5fb818477
konstantin0s <constantintofan85@gmail.com>
2017-12-25 20:49:57 +0100

    Write test association between artist and songs

--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :song do
-       title  { Faker::Name.title }
-      genre   { Faker::Genre.genre }
-
+    
   end
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
+
+
+
   context 'validation tests' do
     it 'ensures name presence' do
       artist = Artist.new(name: 'Pavaroti').save
@@ -12,6 +15,28 @@ RSpec.describe Artist, type: :model do
     it 'should save successfully' do
         end
   end
+
+
+  describe "association with songs" do
+    let(:artist) {create :artist, name: "Livin la vida loca" }
+
+    let!(:song1) { create :song, title: "Song1", genre: "Romance", artist: artist }
+    let!(:song2) { create :song, title: "Song2", genre: "Jazz", artist: artist }
+
+    it "has many songs" do
+      expect(artist.songs).to include(song1)
+      expect(artist.songs).to include(song2)
+    end
+
+    it "deletes associated songs" do
+      expect{ artist.destroy }.to change(Song, :count).by(-2)
+    end
+
+  end
+
+
+
+
 
   context 'scope tests' do
 
