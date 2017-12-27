0e5b2081d5e58f9a8aebd5b351099822ac3da799
konstantin0s <constantintofan85@gmail.com>
2017-12-27 08:17:32 +0100

    change gem file

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-require_relative 'active_record_fk_hack'
+#require_relative 'active_record_fk_hack'
